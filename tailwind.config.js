f726f9c4276875af14ad56d3e5ce0a9e7f655999
khashayar <46204004+khashi1992@users.noreply.github.com>
2024-07-22 21:19:57 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      extend: {
+        fontFamily: {
+          primary: "tello",
+          googel: "sn",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
